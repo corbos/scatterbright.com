fdef2b376dbd856977a20c5669f78e9c49353502
corbos <corbin.march@gmail.com>
2015-01-28 15:33:35 -0600

    Make it white.

--- script/sudoku.js
+++ script/sudoku.js
@@ -1,31 +1,31 @@
-var Sudoku = (function(){
+var Sudoku = (function() {
 
     var sudoku = {};
-    
-    sudoku.section = function(row, col){
+
+    var section = function(row, col) {
         var srow = Math.floor(row / 3) * 3;
         var scol = Math.floor(col / 3);
         return srow + scol;
     };
-    
-    var model = function(){
+
+    var model = function() {
         this.rows = [];
         this.cols = [];
         this.sect = [];
         this.solved = [];
-        for(var i=0;i<9;i++){
+        for(var i = 0; i < 9; i++) {
             this.rows.push([true,true,true,true,true,true,true,true,true]);
             this.cols.push([true,true,true,true,true,true,true,true,true]);
             this.sect.push([true,true,true,true,true,true,true,true,true]);
             this.solved.push([-1,-1,-1,-1,-1,-1,-1,-1,-1]);
         }
     };
-    
+
     model.prototype = {
-        mark:function (row,col,v){	
-            var sect = sudoku.section(row,col);            
+        mark:function (row,col,v) {
+            var sect = section(row,col);
             var result = this.rows[row][v] && this.cols[col][v] && this.sect[sect][v];
-            if(result){
+            if(result) {
                 this.solved[row][col] = v;
                 this.rows[row][v] = false;
                 this.cols[col][v] = false;
@@ -33,38 +33,38 @@ var Sudoku = (function(){
             }
             return result;
         },
-        step:function(index){
+        step:function(index) {
 
             if(index == 81){
                 return true;
             }
-            
+
             var i = Math.floor(index / 9);
             var j = index % 9;
-            
+
             if(this.solved[i][j] > -1){
                 return this.step(index + 1);
             }
-            
-            var sect = sudoku.section(i,j);
-            
-            for(var k=0;k<9;k++){
-                if(this.mark(i,j,k)){
-                    if(this.step(index + 1)){
+
+            var sect = section(i,j);
+
+            for(var k = 0; k < 9; k++) {
+                if(this.mark(i,j,k)) {
+                    if(this.step(index + 1)) {
                         return true;
                     }
-                     //rollback changes		
+                     //rollback changes
                     this.solved[i][j] = -1;
                     this.rows[i][k] = true;
                     this.cols[j][k] = true;
-                    this.sect[sect][k] = true;		
-                }              
+                    this.sect[sect][k] = true;
+                }
             }
             return false;
         }
     };
-    
+
     sudoku.Model = model;
-    
+
     return sudoku;
 }());
--- script/sudokuUI.js
+++ script/sudokuUI.js
@@ -1,33 +1,43 @@
-var Sudoku = (function(sudoku){
+var Sudoku = (function(sudoku) {
 
-    var ui = function(prefix, logFunc){
+    var ui = function(prefix, logFunc) {
         this.prefix = prefix;
         this.log = logFunc;
+        if(typeof logFunc !== "function") {
+            this.log = function(){};
+        }
     };
-    
-    var getModel = function(theUI){
-        var model = new sudoku.Model();
-        for(var i=0;i<9;i++){
-            for(var j=0;j<9;j++){
-                var txt = $('#' + theUI.prefix + i.toString() + '_' + j.toString());
-                if(txt.val().length > 0 && !model.mark(i,j,txt.val() * 1 - 1)){
+
+    var getModel = function(theUI) {
+
+        var model = new sudoku.Model(),
+            i = 0,
+            j = 0,
+            txt;
+
+        for(i = 0; i < 9; i++) {
+            for(j = 0; j < 9; j++) {
+                txt = document.getElementById(theUI.prefix + i.toString() + '_' + j.toString());
+                if(txt.value.length > 0 && !model.mark(i, j, txt.value * 1 - 1)){
                     return null;
                 }
             }
         }
         return model;
     };
-    
+
     ui.prototype = {
-        drawPuzzle:function(){
+
+        drawPuzzle:function() {
+            var i, j, k, l;
             document.write('<table cellpadding="0" cellspacing="0" class="sudoku_table">');
-            for(var i=0;i<3;i++){
+            for(i = 0; i < 3; i++) {
                 document.write('<tr>');
-                for(var j=0;j<3;j++){
+                for(j = 0; j < 3; j++) {
                     document.write('<td class="sudoku_cell">');
-                    for(var k=0;k<3;k++){
+                    for(k = 0; k < 3; k++) {
                         document.write('<div>');
-                        for(var l=0;l<3;l++){
+                        for(l = 0; l < 3; l++) {
                             document.write('<input type="text" class="sudoku_text" id="' + this.prefix + ((i * 3) + k).toString() + '_' + ((j * 3) + l).toString() + '" />');
                         }
                         document.write('</div>');
@@ -38,35 +48,47 @@ var Sudoku = (function(sudoku){
             }
             document.write('</table>');
         },
-        load:function(puzzle){         
-            $('.sudoku_text').val('');
-            for(var i=0;i<9;i++){
-                for(var j =0;j<9;j++){
-                    if(puzzle.charAt(i * 9 + j) != 'X'){
-                        $('#' + this.prefix + i.toString() + '_' + j.toString()).val(puzzle.charAt(i * 9 + j));
+
+        load:function(puzzle) {
+            var i, j, inputs = document.getElementsByTagName('input');
+
+            for(i = 0; i < inputs.length; i++) {
+                if(inputs[i].className === 'sudoku_text') {
+                    inputs[i].value = '';
+                }
+            }
+
+            for(i = 0; i < 9; i++) {
+                for(j = 0; j < 9; j++) {
+                    if(puzzle.charAt(i * 9 + j) !== 'X') {
+                        document.getElementById(this.prefix + i.toString() + '_' + j.toString()).value = puzzle.charAt(i * 9 + j);
                     }
                 }
             }
         },
-        solve:function(){
-            var model = getModel(this);            
+
+        solve:function() {
+
+            var model = getModel(this),
+                i = 0,
+                j = 0,
+                txt;
+
             if(model != null && model.step(0))
             {
-                for(var i=0;i<9;i++){
-                    for(var j=0;j<9;j++){
-                        var txt = $('#' + this.prefix + i.toString() + '_' + j.toString());
-                        txt.val(model.solved[i][j] + 1);
+                for(i = 0; i < 9; i++) {
+                    for(j = 0; j < 9; j++) {
+                        txt = document.getElementById(this.prefix + i.toString() + '_' + j.toString());
+                        txt.value = model.solved[i][j] + 1;
                     }
                 }
+            } else {
+                this.log('The puzzle is unsolvable.');
             }
-            else{
-                if(typeof(this.log) == 'function')
-                    this.log('The puzzle is unsolvable.');
-            }                    
         }
-    };  
+    };
 
     sudoku.UI = ui;
-    
+
     return sudoku;
 }(Sudoku));
